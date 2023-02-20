23c51cc7149895d97c6300d86fe6d6191186bc1d
SunilNagar27 <125652150+SunilNagar27@users.noreply.github.com>
2023-02-20 20:01:07 +0530

    inprogress

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
+const monggose = require('mongoose');
 const app = express ();
 
-app.get("/", (req,resp) =>{
-    resp.send("Api is working .......")
-})
+const dbConnect = async () =>{
+    mongoose.connect('mongodb://localhost:27017/e-comm');
+    const productSchema = mongoose.Schema( {} );
+    const product = mongoose.model("product",productSchema);
+    const data = await product.find();
+    console.log(data);
+}
 
-app.listen(5000);
+
+app.listen(5000); 
+
+dbConnect ();
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 
 const Signup = () => {
+    const [name,setName] = useState ("");
+    const [email,setEmail] = useState ("");
+    const [pass,setPass] = useState ("");
+
+    const collectData = () => {
+        console.warn(name,email,pass);
+    };
+
     return(
         <div className="signup">
             <h3>Signup Page</h3>
-            <input className="inputBox" type="text" placeholder="Enter Name"></input>
-            <input className="inputBox" type="email" placeholder="Enter EMail"></input>
-            <input className="inputBox" type="password" placeholder="password"></input>
+            <input className="inputBox" type="text" value={name} onChange={(e)=>setName(e.target.value) } placeholder="Enter Name"></input>
+            <input className="inputBox" type="email" value={email} onChange={(e)=>setEmail(e.target.value) } placeholder="Enter EMail"></input>
+            <input className="inputBox" type="password" value={pass} onChange={(e)=>setPass(e.target.value) } placeholder="password"></input>
+            <button onClick={collectData} type="button">Singup</button>
         </div>
     )
 };
