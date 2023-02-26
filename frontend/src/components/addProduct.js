51dfa5cf1596f1b23a5bb213dd98ee856fbc760a
SunilNagar27 <125652150+SunilNagar27@users.noreply.github.com>
2023-02-26 10:38:57 +0530

    Validation Update

--- frontend/src/components/addProduct.js
+++ frontend/src/components/addProduct.js
@@ -5,10 +5,16 @@ const AddProduct = () => {
     const [price, setPrice] = useState("");
     const [category, setCategory] = useState("");
     const [company, setCompany] = useState("");
+    const [error,setError] = useState(false)
 
     const productHandler = async () => {
         const userId = JSON.parse(localStorage.getItem('users'))._id;
-        console.warn(name, price, category,userId,company);
+
+        if(!name || !price || !category || !company)
+        {
+            setError(true);
+            return false;
+        }
 
         let data = await fetch("http://localhost:5000/add-product",{
             method: 'post',
@@ -25,9 +31,13 @@ const AddProduct = () => {
         <div className='login'>
             <h2>Add Product</h2>
             <input className='inputBox' value={name} onChange={(e) =>{ setName(e.target.value)}} type='text' placeholder='Enter Name' />
+            { error && !name && <span className='invalid-input'>Enter Valid Name</span>}
             <input className='inputBox' value={price} onChange={(e) => {setPrice(e.target.value)}} type='text' placeholder='Enter Price' />
+            { error && !price && <span className='invalid-input'>Enter Valid Price</span>}
             <input className='inputBox' value={category} onChange={(e) => {setCategory(e.target.value)}} type='text' placeholder='Enter category' />
+            { error && !category && <span className='invalid-input'>Enter Valid category</span>}
             <input className='inputBox' value={company} onChange={(e) => {setCompany(e.target.value)}} type='text' placeholder='Enter Company' />
+            { error && !company && <span className='invalid-input'>Enter Valid Company</span>}
             <button onClick={productHandler} className="BUTTON" type='button' > Add Product </button>
         </div>
     )
